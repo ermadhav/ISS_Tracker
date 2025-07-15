99c02992d4853c078709319bfcc9ea2188c74f7e
ermadhav <madhavtiwari2024@gmail.com>
2025-07-15 23:50:26 +0530

    Solved

--- server/server.js
+++ server/server.js
@@ -16,11 +16,8 @@ app.use(express.json()); // Parse JSON request bodies
 
 
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 

733fb7d0829ebfe4b3947948c8618bcb3afa47c0
ermadhav <madhavtiwari2024@gmail.com>
2025-07-15 23:52:58 +0530

    Solved

--- server/server.js
+++ server/server.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS for all origins (adjust for production)
 app.use(express.json()); // Parse JSON request bodies
 
-
-
 mongoose.connect(process.env.MONGO_URI)
 
 .then(() => console.log('MongoDB connected successfully'))

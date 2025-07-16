b0b5abae86be2429b769b97ae342733854e574fc
ermadhav <madhavtiwari2024@gmail.com>
2025-07-16 23:08:27 +0530

    UPdated

--- server/utils/cronJobs.js
+++
@@ -1,9 +0,0 @@
-import cron from 'node-cron';
-import { checkAndSendAlerts } from '../controllers/issController.js';
-
-export const startAlertScheduler = () => {
-  cron.schedule('*/10 * * * *', () => {
-    console.log('Running scheduled ISS alert check...');
-    checkAndSendAlerts();
-  });
-};

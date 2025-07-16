d54df8daba24de6a3ed0d5f4e0afed57010232e0
ermadhav <madhavtiwari2024@gmail.com>
2025-07-17 00:12:51 +0530

    UPdated

--- client/src/components/GlobeView.js
+++ client/src/components/GlobeView.js
@@ -25,7 +25,7 @@ export default function ISSDashboard() {
         altitude: data.altitude || 419.06,
       });
 
-      setPath(prev => [...prev.slice(-19), { lat, lng }]);
+      setPath(prev => [...prev.slice(-19), [lat, lng]]);
 
       if (globeRef.current) {
         globeRef.current.pointOfView({ lat, lng, altitude: 1.5 }, 1000);
@@ -36,16 +36,6 @@ export default function ISSDashboard() {
     const interval = setInterval(fetchISS, 5000);
     return () => clearInterval(interval);
   }, []);
-
-  const lastSegment = path.length >= 2
-    ? [{
-        startLat: path[path.length - 2].lat,
-        startLng: path[path.length - 2].lng,
-        endLat: path[path.length - 1].lat,
-        endLng: path[path.length - 1].lng,
-        color: ['#ffcc00'],
-      }]
-    : [];
 
   return (
     <>
@@ -113,13 +103,10 @@ export default function ISSDashboard() {
         }
         .globe-wrapper {
           flex-grow: 1;
-          position: relative;
-        }
-        .globe-wrapper canvas {
           border-radius: 0 15px 15px 0;
           box-shadow: inset 0 0 40px #ffc107aa;
+          overflow: hidden;
         }
-        /* ISS Icon glow */
         .iss-icon {
           filter: drop-shadow(0 0 6px #ffc107);
         }
@@ -158,7 +145,13 @@ export default function ISSDashboard() {
             ref={globeRef}
             globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
             backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-            arcsData={lastSegment}
+            arcsData={path.length >= 2 ? [{
+              startLat: path[path.length - 2][0],
+              startLng: path[path.length - 2][1],
+              endLat: path[path.length - 1][0],
+              endLng: path[path.length - 1][1],
+              color: ['#ffcc00'],
+            }] : []}
             arcColor={() => '#ffc107'}
             arcAltitude={0.05}
             arcStroke={2}
@@ -166,15 +159,11 @@ export default function ISSDashboard() {
             arcDashGap={2}
             arcDashInitialGap={() => Math.random() * 5}
             arcDashAnimateTime={1000}
-
-            // Remove default points
             pointsData={[]}
-
-            // Render ISS icon as HTML element on globe
             htmlElementsData={[issData]}
             htmlElement={(d) => {
               const el = document.createElement('img');
-              el.src = '/iss-icon.png'; // Make sure iss-icon.png is in public folder
+              el.src = '/iss-icon.png';
               el.style.width = '50px';
               el.style.height = '50px';
               el.className = 'iss-icon';
